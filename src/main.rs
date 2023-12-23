35cc1d34d12b59cdce51ff7f666b9900cf993e27
Philipp Paller <philipp.paller.ds@gmail.com>
2023-12-23 21:26:31 +0100

    visual improval and calculation of workdays

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::env;
 
+macro_rules! vec_of_strings {
+    ($($x:expr),*) => (vec![$($x.to_string()),*]);
+}
+
 fn help() {
     println!("usage:
 ./should_i_automate.exe <years> {{years: how long it will have to be done manually before a new solution would make it obsolete}}");
@@ -13,8 +17,12 @@ fn compute_time_spent(days:u32, occurence_per_day: f32, time_for_event: u32) -> 
     let result = ((time_for_event as f32) * occurence_per_day * (days as f32)) as i64;
     let seconds = result % 60;
     let minutes = (result / 60) % 60;
-    let hours = (result / 60) / 60;
-    let return_string = format!("{:0>2}:{:0>2}:{:0>2}", hours, minutes, seconds);
+    let hours = ((result / 60) / 60) %8;
+    let days = ((result / 60) / 60) / 8;
+    let mut return_string = format!("{:0>2} workdays {:0>2}:{:0>2}:{:0>2}", days, hours, minutes, seconds);
+    if ((occurence_per_day * time_for_event as f32) as i64) > (8*60*60) {
+        return_string = "-".to_owned();
+    }
     return return_string
 }
 
@@ -23,17 +31,18 @@ fn compute_automation_time(years: u16) {
     let days: u32 = (years * 365).into();
 
     let times_per_event = [5, 30, 60, 2*60, 5*60, 15*60, 30*60, 60*60, 2*60*60, 4*60*60];
+    let occurence_labels: Vec<String> = vec_of_strings!["5 Seconds", "30 Seconds", "1 Minute", "2 Minutes", "5 Minutes", "15 Minutes", "30 Minutes", "1 Hour", "2 Hours", "4 Hours"];
     let occurences_per_day: [f32; 7] = [50.0, 10.0, 1.0, 2.0/7.0, 1.0/7.0, 2.0/(7.0 * 4.33), 1.0/(7.0 * 4.33)];
     println!("{:?}", times_per_event);
     println!("{:?}", occurences_per_day);
 
-    println!("|{:>10}|{:>10}|{:>10}|{:>10}|{:>10}|{:>10}|{:>10}|", "50/day", "10/day", "daily", "bi-weekly", "weekly", "bi-monthly", "monthly");
-    for time in times_per_event {
+    println!("|{:>25}|{:>25}|{:>25}|{:>25}|{:>25}|{:>25}|{:>25}|{:>25}|", "-", "50/day", "10/day", "daily", "bi-weekly", "weekly", "bi-monthly", "monthly");
+    for (occurence_label_iter, time) in times_per_event.iter().enumerate() {
         let mut time_spent: Vec<String> = vec![String::new(); 7];
         for (i, occurencies) in occurences_per_day.iter().enumerate() {
-            time_spent[i] = compute_time_spent(days, *occurencies, time);
+            time_spent[i] = compute_time_spent(days, *occurencies, *time);
         }
-        println!("|{:>10}|{:>10}|{:>10}|{:>10}|{:>10}|{:>10}|{:>10}|", time_spent[0], time_spent[1], time_spent[2], time_spent[3], time_spent[4], time_spent[5], time_spent[6]);        
+        println!("|{:>25}|{:>25}|{:>25}|{:>25}|{:>25}|{:>25}|{:>25}|{:>25}|", occurence_labels[occurence_label_iter], time_spent[0], time_spent[1], time_spent[2], time_spent[3], time_spent[4], time_spent[5], time_spent[6]);        
     }
     
 }
